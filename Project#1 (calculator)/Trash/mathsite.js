0a104190f000b7d510f026f159b48d2c37b8ecdc
ChaoticHat <sandro.metreveli12@gmail.com>
2021-07-17 18:26:53 +0400

    გადმოვიტანე აქ {საფინალო ვერსია მალე იქნება}

---
+++ Project#1 (calculator)/Trash/mathsite.js
@@ -0,0 +1,138 @@
+
+function ricxvebisjami(){
+    var masivi = [];
+    var jami = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1];
+    var jami = pirveliEl + meoreEl;
+    
+    alert('თქვენს მიერ შემოყვანილი რიცხვებია' + masivi + '; მათი ჯამი კი' + jami);
+}
+function sxvaoba(){
+    var masivi = [];
+    var sxvaoba = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1];
+    var sxvaoba = pirveliEl - meoreEl;
+    
+    alert('თქვენს მიერ შემოყვანილი რიცხვებია' + masivi + '; მათი სხვაობა კი' + sxvaoba);
+}
+function namravli(){
+    var masivi = [];
+    var namravli = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1];
+    var namravli = pirveliEl * meoreEl;
+    alert('თქვენს მიერ შემოყვანილი რიცხვებია' + masivi + '; მათი ნამრავლი კი' + namravli);
+}
+function ganayofi(){
+    var masivi = [];
+    var ganayofi = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1];
+    var ganayofi = pirveliEl / meoreEl;
+    alert('თქვენს მიერ შემოყვანილი რიცხვებია' + masivi + '; მათი განაყოფი კი' + ganayofi);
+}
+function kubshiayvana(){
+    var masivi = [];
+    var kubi = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ 1 რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var kubi = pirveliEl ** 3;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ეს რიცხვი რომ კუბში ავიყვანოთ გამოვა-' + kubi);
+}
+function kvadrati(){
+    var masivi = [];
+    var kvadrati = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ 1 რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var kvadrati = pirveliEl ** 2;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ეს რიცხვი რომ კვადრატში ავიყვანოთ გამოვა-' + kvadrati);
+}
+function ab2(){
+    var masivi = [];
+    var a2b2 = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1]
+    var a2b2 = pirveliEl ** 2 + meoreEl ** 2;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ამ რიცხვების კვადრატების ჯამი კი-' + a2b2);
+}
+function kvsxvaoba(){
+    var masivi = [];
+    var kvsxvaoba = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1]
+    var kvsxvaoba = pirveliEl ** 2 - meoreEl ** 2;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ამ რიცხვების კვადრატების სხვაობა კი-' + kvsxvaoba);
+}
+function kubjami(){
+    var masivi = [];
+    var kubjami = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1]
+    var kubjami = pirveliEl ** 3 + meoreEl ** 3;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ამ რიცხვების კუბების ჯამი კი-' + kubjami);
+}
+function kubsxvaoba(){
+    var masivi = [];
+    var kubsxvaoba = 0;
+    for(var i = 0; i < 1; i++){
+        var el = parseInt(prompt('შემოიყვანეთ  პირველი რიცხვი'));
+        masivi.push(el);
+        var el = parseInt(prompt('შემოიყვანეთ  მეორე რიცხვი'));
+        masivi.push(el);
+    }
+    var pirveliEl = masivi[0];
+    var meoreEl = masivi[1]
+    var kubsxvaoba = pirveliEl ** 3 - meoreEl ** 3;
+    alert('თქვენს მიერ შემოყვანილი რიცხვია' + masivi + '; ამ რიცხვების კუბების სხვაობა კი-' + kubsxvaoba);
+}
+    
